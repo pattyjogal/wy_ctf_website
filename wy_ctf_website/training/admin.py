e0b6964560c85356f3ece223c73ab2d288ad5fcb
Patrick Gallagher <patrickj@cpgallagher.com>
2016-10-14 09:22:56 -0500

    Score Change

--- wy_ctf_website/training/admin.py
+++ wy_ctf_website/training/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Challenge, ChallengeAdmin, Solution
+from .models import Challenge, ChallengeAdmin, Solution, Score
 
 # Register your models here.
 
 admin.site.register(Challenge, ChallengeAdmin)
 admin.site.register(Solution)
+admin.site.register(Score)
